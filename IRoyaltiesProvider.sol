75974fea5df49c0287080d3beb1a78c5399987bd
R3LYC <92279501+R3LYC@users.noreply.github.com>
2021-12-06 23:09:56 +1300

    Update IRoyaltiesProvider.sol

--- IRoyaltiesProvider.sol
+++ IRoyaltiesProvider.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0; 
 
-import "@R3LYC/contracts/blob/main/LibPart.sol"; interface IRoyaltiesProvider {
+import "https://github.com/R3LYC/contracts/blob/c29b063e595a1d2aa7e0a8d02bdcb5c1fa5720b5/LibPart.sol"; interface IRoyaltiesProvider {
 
     function getRoyalties(addres token, uint tokenId) external returns (LibPart.[]memory);
 }
