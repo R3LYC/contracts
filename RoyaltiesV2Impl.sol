3530846830470fd88ec42db23e46b10de2c76e06
R3LYC <92279501+R3LYC@users.noreply.github.com>
2021-12-06 23:11:46 +1300

    Update RoyaltiesV2Impl.sol

--- RoyaltiesV2Impl.sol
+++ RoyaltiesV2Impl.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/R3LYC/contracts/blob/2989aa2a4b5a3d0d3e2a096342faa4e8c3c0cf41/AbstractRoyalties.sol";
 
-import "@R3LYC/contracts/blob/main/RoyaltiesV2.sol";
+import "https://github.com/R3LYC/contracts/blob/305f28a72c21dc2dbfbc7f8d913586bc90bc5fac/RoyaltiesV2.sol";
 
 contract RoyaltiesV2Impl is AbstractRoyalties, RoyaltiesV2 
 {
